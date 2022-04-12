04c232775cae6eaa62094aa579bfcc35979b8a60
Duc Nguyen <duc.ngnguyen@gmail.com>
2022-04-12 12:36:37 +0200

    Compare results and log them

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
+let roundWinner = '';
+let playerScore = 0;
+let computerscore = 0;
+
+
 function computerPlay() {
-    const computerChoices = ['Rock', 'Paper', 'Scissor'];
+    const computerChoices = ['rock', 'paper', 'scissor'];
     const random = Math.floor(Math.random() * computerChoices.length)
-
-    console.log(computerChoices[random])
-
     return computerChoices[random]
 }
 
-computerPlay();
+function playRound(computerSelection, playerSelection) {
+
+    const playerSelectionLow = playerSelection.toLowerCase();
+
+    if (computerSelection === playerSelectionLow) roundWinner = 'tie';
+
+    if (
+        computerSelection === 'rock' && playerSelectionLow === 'scissor' ||
+        computerSelection === 'paper' && playerSelectionLow === 'rock' ||
+        computerSelection === 'scissor' && playerSelectionLow === 'paper') {
+        roundWinner = 'computer';
+    }
+
+    if (
+        computerSelection === 'rock' && playerSelectionLow === 'paper' ||
+        computerSelection === 'paper' && playerSelectionLow === 'scissor' ||
+        computerSelection === 'scissor' && playerSelectionLow === 'rock') {
+        roundWinner = 'player';
+    }
+
+    outputResult(roundWinner, computerSelection, playerSelectionLow)
+
+}
+
+function outputResult(winner, comSelection, playerSelection) {
+    if (winner === 'player') {
+        console.log(`You Win! ${playerSelection} gets beaten by ${comSelection}`)
+    }
+    
+    
+    if (winner === 'computer') {
+        console.log(`You Lose! ${playerSelection} beats ${comSelection}`)
+    }
+
+}
+
+const playerSelection = 'Rock';
+const computerselection = computerPlay();
+
+playRound(computerselection, playerSelection)
