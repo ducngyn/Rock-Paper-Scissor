06847b688c334ccf20596e064b3aa50c4ecb73c1
Duc Nguyen <duc.ngnguyen@gmail.com>
2022-04-14 12:29:37 +0200

    Add UI and delete hello-world.txt

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-let roundWinner = '';
 let playerScore = 0;
 let computerscore = 0;
+let roundWinner = '';
+const playerScoreObject = document.querySelector('.js-player-score');
+const comScoreObject = document.querySelector('.js-com-score');
+
 
 function computerPlay() {
-    const computerChoices = ['rock', 'paper', 'scissor'];
+    const computerChoices = ['Rock', 'Paper', 'Scissor'];
     const random = Math.floor(Math.random() * computerChoices.length);
-
     return computerChoices[random];
 }
 
 function playRound(computerSelection, playerSelection) {
-    const playerSelectionLow = playerSelection.toString().toLowerCase();
     const playerWins =
-    computerSelection === 'rock' && playerSelectionLow === 'paper' ||
-    computerSelection === 'paper' && playerSelectionLow === 'scissor' ||
-    computerSelection === 'scissor' && playerSelectionLow === 'rock';
+        computerSelection === 'Rock' && playerSelection === 'Paper' ||
+        computerSelection === 'Paper' && playerSelection === 'Scissor' ||
+        computerSelection === 'Scissor' && playerSelection === 'Rock';
     const computerWins =
-    computerSelection === 'rock' && playerSelectionLow === 'scissor' ||
-    computerSelection === 'paper' && playerSelectionLow === 'rock' ||
-    computerSelection === 'scissor' && playerSelectionLow === 'paper';
-    
-    if (!['rock', 'paper', 'scissor'].includes(playerSelectionLow)) {
-        console.log('invalid value');
-        return;
-    }
-    
-    if (computerSelection === playerSelectionLow) roundWinner = 'tie';
+        computerSelection === 'Rock' && playerSelection === 'Scissor' ||
+        computerSelection === 'Paper' && playerSelection === 'Rock' ||
+        computerSelection === 'Scissor' && playerSelection === 'Paper';
+
+
+    if (computerSelection === playerSelection) roundWinner = 'tie';
 
     if (computerWins) {
         roundWinner = 'computer';
@@ -37,34 +34,55 @@ function playRound(computerSelection, playerSelection) {
         playerScore++;
     }
 
-    outputResult(roundWinner, computerSelection, playerSelectionLow);
+    outputResult(roundWinner, computerSelection, playerSelection);
 }
 
 function outputResult(winner, comSelection, playerSelection) {
-    if (winner === 'player') {
-        console.log(`You Win! ${playerSelection} beats ${comSelection}`)
-    }
+    const comChoice = document.querySelector('.js-com-choice');
+    comChoice.innerText = comSelection;
 
-    if (winner === 'computer') {
-        console.log(`You Lose! ${playerSelection} gets beaten ${comSelection}`)
-    }
+    const result = document.querySelector('.js-result');
+    if (winner === 'player') result.innerText = `You Win! ${playerSelection} beats ${comSelection}`;
+    if (winner === 'computer') result.innerText = `You Lose! ${playerSelection} gets beaten ${comSelection}`;
+    if (winner === 'tie') result.innerText = `You both tied`;
 
-    if (winner === 'tie') {
-        console.log(`You both tied`)
-    }
+    updateScore();
+    checkForWinner();
 }
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = window.prompt('please input a value');
-        const computerselection = computerPlay();
-        playRound(computerselection, playerSelection)
+
+function initButtons() {
+    const choices = document.querySelectorAll('.choice');
+
+    choices.forEach(choice => {
+        choice.addEventListener('click', (e) => {
+            playRound(computerPlay(), e.target.innerText)
+        })
+    });
+}
+
+function updateScore() {
+    playerScoreObject.innerText = playerScore;
+    comScoreObject.innerText = computerscore;
+}
+
+function checkForWinner() {
+
+    const finalScore = document.querySelector('.js-final-score');
+
+    if ((playerScore === 5) && (playerScore > computerscore)) {
+        finalScore.innerText = `You win with ${playerScore} point(s), while the computer has ${computerscore} point(s)`;
     }
 
-    if (playerScore > computerscore) console.log(`You win with ${playerScore} point(s), while the computer has ${computerscore} point(s)`)
-    if (playerScore < computerscore) console.log(`You lose with ${playerScore} point(s), while the computer has ${computerscore} point(s)`)
-    if (playerScore > computerscore) console.log(`You tie with ${playerScore} point(s)`)
+    if ((playerScore < computerscore) && (computerscore === 5)) {
+       finalScore.innerText = `You lose with ${playerScore} point(s), while the computer has ${computerscore} point(s)`;
+    }
+}
+
+function game() {
+    initButtons();
+    updateScore();
 }
 
 game();
